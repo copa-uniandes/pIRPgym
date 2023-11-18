d025ff38855b70483a9e86d12b8f5369ae0100bd
juanbeta98 <juan.beta98@gmail.com>
2023-11-18 16:24:58 -0500

    Adapted the alpha for the classic instances

--- pIRPgym/Blocks/Policies/Routing.py
+++ pIRPgym/Blocks/Policies/Routing.py
@@ -1095,7 +1095,7 @@ class Routing():
 
 
         @staticmethod
-        def evaluate_stochastic_policy(router,purchase,inst_gen:instance_generator,env,n=30,averages=True,dynamic_p=False)->tuple:
+        def evaluate_stochastic_policy(router,purchase,inst_gen:instance_generator,env,n=30,averages=True,dynamic_p=False,**kwargs)->tuple:
             times = list()
             vehicles = list()
             objectives = list()
@@ -1105,7 +1105,7 @@ class Routing():
             if router == Routing.RCL_Heuristic:
                 for i in range(n):
                     seed = i*2
-                    RCL_routes,RCL_obj,RCL_info,RCL_time  = router(purchase,inst_gen,env.t,RCL_alpha=0.25,rd_seed=seed,time_limit=5)
+                    RCL_routes,RCL_obj,RCL_info,RCL_time  = router(purchase,inst_gen,env.t,RCL_alpha=0.05,rd_seed=seed,time_limit=5)
                     times.append(RCL_time)
                     vehicles.append(len(RCL_routes))
                     objectives.append(RCL_obj)
