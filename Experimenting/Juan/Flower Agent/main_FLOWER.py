36bd09577b7058c24d141f632abcffe66bc69ebb
juanbeta98 <juan.beta98@gmail.com>
2023-12-29 01:10:29 -0500

    Flower Agent update

--- Experimenting/Juan/Flower Agent/main_FLOWER.py
+++ Experimenting/Juan/Flower Agent/main_FLOWER.py
@@ -33,7 +33,7 @@ historical_data = ['*']
 # Other parameters
 backorders = 'backorders'
 
-sizes = [5,10,15,20]
+sizes = [5,10,15,20,30]
 env_config = {'T':12,'Q':750,
               'S':3,'LA_horizon':3,
              'd_max':2500,'hist_window':60,
@@ -67,12 +67,12 @@ seeds = []
 FlowerAgent = pIRPgym.FlowerAgent(solution_num=25)
 main_done = False
 ep_count = 0
-num_episodes = 30
+num_episodes = 10
 
 
 
 
-env_config['M']=sizes[3]
+env_config['M']=sizes[4]
 env_config['K']=env_config['M']
 env_config['F']=env_config['M']
 det_rd_seed = env_config['K']             # Random seeds
